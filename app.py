9d6b5882703b9c8b51349818ca15255d9a27e38e
astha0202 <astha.dwivedi0202@gmail.com>
2026-05-10 20:47:37 +0530

    fixed railway environment variables

--- app.py
+++ app.py
@@ -1,39 +1,68 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+import os
 
-from database.db import mysql
+# =========================
+# IMPORT ROUTES
+# =========================
 
-from routes.admin_routes import admin_bp
-
-# 🔹 Import all routes
 from routes.main_routes import main_bp
 from routes.shop_routes import shop_bp
 from routes.cart_routes import cart_bp
 from routes.payment_routes import payment_bp
+from routes.admin_routes import admin_bp
 
-# 🔹 Create app
+# =========================
+# CREATE APP
+# =========================
+
 app = Flask(__name__)
+
 app.secret_key = "secret123"
-# MYSQl CONFIG
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'mahoba_market'
-# INIT MYSQL
-mysql.init_app(app)
+
+# =========================
+# MYSQL CONFIG
+# =========================
+
+app.config['MYSQL_HOST'] = os.environ.get("MYSQLHOST")
+
+app.config['MYSQL_USER'] = os.environ.get("MYSQLUSER")
+
+app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQLPASSWORD")
+
+app.config['MYSQL_DB'] = os.environ.get("MYSQLDATABASE")
+
+app.config['MYSQL_PORT'] = int(
+    os.environ.get("MYSQLPORT", 3306)
+)
+
+# =========================
+# MYSQL INIT
+# =========================
+
+mysql = MySQL(app)
+
+# =========================
 # REGISTER BLUEPRINTS
+# =========================
+
 app.register_blueprint(main_bp)
+
 app.register_blueprint(shop_bp)
+
 app.register_blueprint(cart_bp)
+
 app.register_blueprint(payment_bp)
+
 app.register_blueprint(admin_bp)
 
+# =========================
 # RUN APP
+# =========================
 
 if __name__ == "__main__":
 
-    import os
-
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT",5000))
+        port=int(os.environ.get("PORT", 5000))
     )
